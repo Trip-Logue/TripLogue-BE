827c206eada1e4a3474e978e7728a7fd6fe75091
enohs <kmsn01@naver.com>
2025-06-27 14:14:48 +0900

    :tada: init: start project

---
+++ src/main/java/com/triploguebe/TripLogueBeApplication.java
@@ -0,0 +1,13 @@
+package com.triploguebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripLogueBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TripLogueBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/triploguebe/TripLogueBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triploguebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripLogueBeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
