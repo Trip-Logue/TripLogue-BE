0f6bc54cafd9043e10273b9b5c4b9cbcae7297bb
정윤주 <oyunjooya@naver.com>
2025-07-05 19:01:26 +0900

    Feat: Photo 생성

--- src/main/java/com/triploguebe/photo/entity/Photo.java
+++ src/main/java/com/triploguebe/photo/entity/Photo.java
@@ -1,4 +1,22 @@
 package com.triploguebe.photo.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Photo {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long photoId;
+
+    private String imageUrl;
+    private LocalDate uploadedDate;
+
 }
+
