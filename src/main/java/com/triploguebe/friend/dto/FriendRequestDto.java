423604035ad28b34ee46d6290b3eeb0003b48b41
정윤주 <oyunjooya@naver.com>
2025-07-05 17:41:48 +0900

    Feat: FriendRequestDto 생성

--- src/main/java/com/triploguebe/friend/dto/FriendRequestDto.java
+++ src/main/java/com/triploguebe/friend/dto/FriendRequestDto.java
@@ -1,4 +1,13 @@
 package com.triploguebe.friend.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FriendRequestDto {
+    private Long userId;    // 친구 요청을 보낸 사람 ID
+    private Long friendId;  // 친구 요청 받을 사람 ID
 }
+
