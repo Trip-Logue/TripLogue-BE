40b4122645920179b56bafc64e5264388c239ef9
정윤주 <oyunjooya@naver.com>
2025-07-05 19:02:11 +0900

    Feat: PhotoUpdateRequest 생성

--- src/main/java/com/triploguebe/photo/dto/PhotoUpdateRequest.java
+++ src/main/java/com/triploguebe/photo/dto/PhotoUpdateRequest.java
@@ -1,4 +1,11 @@
 package com.triploguebe.photo.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhotoUpdateRequest {
+    private String imageUrl;
 }
