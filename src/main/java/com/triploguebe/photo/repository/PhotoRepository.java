947bab4519e61f86ae9a8b0797a15635f020f6d3
정윤주 <oyunjooya@naver.com>
2025-07-05 19:01:44 +0900

    Feat: PhotoRepository 생성

--- src/main/java/com/triploguebe/photo/repository/PhotoRepository.java
+++ src/main/java/com/triploguebe/photo/repository/PhotoRepository.java
@@ -1,4 +1,10 @@
 package com.triploguebe.photo.repository;
 
-public class PhotoRepository {
+import com.triploguebe.photo.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
 }
