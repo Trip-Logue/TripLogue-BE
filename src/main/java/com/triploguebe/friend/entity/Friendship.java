3b207a17c3c5044e6f9c7d66da7912e6ee1c563b
정윤주 <oyunjooya@naver.com>
2025-07-05 17:34:46 +0900

    Feat: Friendship 생성

--- src/main/java/com/triploguebe/friend/entity/Friendship.java
+++ src/main/java/com/triploguebe/friend/entity/Friendship.java
@@ -1,4 +1,34 @@
 package com.triploguebe.friend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Friendship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long friendshipId;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime requestDate;
+
+    // 친구 요청 보낸 사람
+    private Long userId;
+
+    // 친구 요청 받은 사람
+    private Long friendId;
+
+    public enum Status {
+        PENDING, ACCEPTED, REJECTED, DELETED
+    }
 }
