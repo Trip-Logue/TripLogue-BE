6e7ab115bc7bc0c89a39efa3716e7d66b83b867a
정윤주 <oyunjooya@naver.com>
2025-07-06 23:20:04 +0900

    Feat: TripService 생성

--- src/main/java/com/triploguebe/trip/service/TripService.java
+++ src/main/java/com/triploguebe/trip/service/TripService.java
@@ -1,4 +1,18 @@
 package com.triploguebe.trip.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.triploguebe.trip.dto.*;
+import com.triploguebe.trip.entity.*;
+import com.triploguebe.trip.repository.*;
+
+@Service
+@RequiredArgsConstructor
 public class TripService {
+    private final TripLogRepository tripLogRepository;
+    private final TripLocationRepository tripLocationRepository;
+
+    public TripResponse createTrip(TripCreateRequest request) {
+        return null;
+    }
 }
