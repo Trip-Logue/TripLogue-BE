7aff6c34e0c46018b765b1a3a89435d28f32aac7
정윤주 <oyunjooya@naver.com>
2025-07-06 23:19:54 +0900

    Feat: TripUpdateRequest 생성

--- src/main/java/com/triploguebe/trip/dto/TripUpdateRequest.java
+++ src/main/java/com/triploguebe/trip/dto/TripUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.triploguebe.trip.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TripUpdateRequest {
+    private String title;
+    private String description;
 }
+
