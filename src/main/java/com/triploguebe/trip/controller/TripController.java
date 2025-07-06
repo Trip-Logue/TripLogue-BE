fab62fc460b8eb0a0d5c05d5643c3a40e450ed81
정윤주 <oyunjooya@naver.com>
2025-07-06 23:20:13 +0900

    Feat: TripController 생성

--- src/main/java/com/triploguebe/trip/controller/TripController.java
+++ src/main/java/com/triploguebe/trip/controller/TripController.java
@@ -1,4 +1,19 @@
 package com.triploguebe.trip.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import com.triploguebe.trip.service.TripService;
+import com.triploguebe.trip.dto.*;
+
+@RestController
+@RequestMapping("/api/trips")
+@RequiredArgsConstructor
 public class TripController {
+
+    private final TripService tripService;
+
+    @PostMapping
+    public TripResponse createTrip(@RequestBody TripCreateRequest request) {
+        return tripService.createTrip(request);
+    }
 }
