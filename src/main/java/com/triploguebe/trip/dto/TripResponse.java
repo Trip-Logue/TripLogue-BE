f0192b94c4c60e85c8508e1b6f4bf6fbf1c2806a
정윤주 <oyunjooya@naver.com>
2025-07-06 23:19:45 +0900

    Feat: TripResponse 생성

--- src/main/java/com/triploguebe/trip/dto/TripResponse.java
+++ src/main/java/com/triploguebe/trip/dto/TripResponse.java
@@ -1,4 +1,16 @@
 package com.triploguebe.trip.dto;
 
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TripResponse {
+    private Long triplogId;
+    private String title;
+    private String description;
+    private LocalDate visitedDate;
+    private String placeName;
 }
