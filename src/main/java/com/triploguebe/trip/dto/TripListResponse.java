df6c389d0aac26c4fcaf7e4e083d4eea80118687
정윤주 <oyunjooya@naver.com>
2025-07-06 23:19:30 +0900

    Feat: TripListResponse 생성

--- src/main/java/com/triploguebe/trip/dto/TripListResponse.java
+++ src/main/java/com/triploguebe/trip/dto/TripListResponse.java
@@ -1,4 +1,12 @@
 package com.triploguebe.trip.dto;
 
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TripListResponse {
+    private List<TripResponse> trips;
 }
