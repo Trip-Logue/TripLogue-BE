c62a88cab4ffbafb4cf478b17b84ed7b37a3f888
정윤주 <oyunjooya@naver.com>
2025-07-06 23:17:28 +0900

    Feat: TripLog 생성

--- src/main/java/com/triploguebe/trip/entity/TripLog.java
+++ src/main/java/com/triploguebe/trip/entity/TripLog.java
@@ -1,4 +1,21 @@
 package com.triploguebe.trip.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TripLog {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long triplogId;
+
+    private String title;
+    private String description;
+    private LocalDate createdDate;
+    private LocalDate visitedDate;
 }
