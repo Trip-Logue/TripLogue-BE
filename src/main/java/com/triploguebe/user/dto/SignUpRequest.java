4c104ec21c818bd00ac3f474e7f07fab326c378a
Hyemin Seo <hhese89985@gmail.com>
2025-07-07 15:55:36 +0900

    Feat: SignUpRequest 생성

--- src/main/java/com/triploguebe/user/dto/SignUpRequest.java
+++ src/main/java/com/triploguebe/user/dto/SignUpRequest.java
@@ -1,4 +1,12 @@
 package com.triploguebe.user.dto;
 
+import lombok.Data;
+
+@Data
 public class SignUpRequest {
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
 }
+
