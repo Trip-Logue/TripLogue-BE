fc14e17e53931f2e411feb62f8776d32ca5187b7
Hyemin Seo <hhese89985@gmail.com>
2025-07-07 15:56:36 +0900

    Feat: CustomUserDetailsService 생성

--- src/main/java/com/triploguebe/user/security/CustomUserDetailsService.java
+++ src/main/java/com/triploguebe/user/security/CustomUserDetailsService.java
@@ -1,4 +1,17 @@
 package com.triploguebe.user.security;
 
-public class CustomUserDetailsService {
+import com.triploguebe.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    //추가
 }
+
