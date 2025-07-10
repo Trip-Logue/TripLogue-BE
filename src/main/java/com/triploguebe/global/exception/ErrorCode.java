bbc8e487953b300e444f583e91428a0c3cee915a
정윤주 <oyunjooya@naver.com>
2025-07-10 14:18:19 +0900

    Feat: ErrorCode에 새로운 예외 코드 추가

--- src/main/java/com/triploguebe/global/exception/ErrorCode.java
+++ src/main/java/com/triploguebe/global/exception/ErrorCode.java
@@ -10,7 +10,12 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     UNAUTHORIZED(401, "인증되지 않았습니다."),
     FORBIDDEN(403, "권한이 없습니다."),
-    BAD_REQUEST(400, "잘못된 요청입니다.");
+    BAD_REQUEST(400, "잘못된 요청입니다."),
+    // 회원가입 관련
+    DUPLICATE_USERNAME(400, "이미 존재하는 사용자 이름입니다."),
+    DUPLICATE_EMAIL(400, "이미 존재하는 이메일입니다."),
+    // 비밀번호
+    INVALID_PASSWORD(400, "기존 비밀번호가 일치하지 않습니다.");
 
     private final int status;
     private final String message;
