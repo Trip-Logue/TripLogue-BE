2384b1121cc1f0ce3183ebfb5c2be786404c764c
정윤주 <oyunjooya@naver.com>
2025-07-29 19:02:14 +0900

    Chore: 불필요한 import 제거

--- src/main/java/com/triploguebe/user/controller/UserController.java
+++ src/main/java/com/triploguebe/user/controller/UserController.java
@@ -13,8 +13,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
