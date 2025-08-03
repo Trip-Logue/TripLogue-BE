2a0a70b210e2f1885acfb040a3a4934a73c8e80f
정윤주 <oyunjooya@naver.com>
2025-08-03 19:41:48 +0900

    Fix: JPA 쿼리 생성 오류 해결

--- src/main/java/com/triploguebe/friend/repository/FriendshipRepository.java
+++ src/main/java/com/triploguebe/friend/repository/FriendshipRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
     // 친구 요청 수락, 거절할 때 해당 요청이 존재하는지, 아직 PENDING 상태인지 확인
-    Optional<Friendship> findByIdAndStatus(Long id, FriendshipStatus status);
+    Optional<Friendship> findByFriendshipIdAndStatus(Long friendshipId, FriendshipStatus status);
 
     // 내가 받은 요청 중 아직 대기 중인(PENDING) 요청들 조회
     List<Friendship> findAllByFriendIdAndStatus(Long FriendId, FriendshipStatus status);
