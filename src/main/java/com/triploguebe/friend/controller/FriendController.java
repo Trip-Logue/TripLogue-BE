abf140be54a5a9f6d32c0fd505123f2fc023b6b4
정윤주 <oyunjooya@naver.com>
2025-08-08 21:12:57 +0900

    Chore: 불필요한 import 제거

--- src/main/java/com/triploguebe/friend/controller/FriendController.java
+++ src/main/java/com/triploguebe/friend/controller/FriendController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
